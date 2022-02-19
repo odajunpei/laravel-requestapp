022c6618c5232ef6156690a1fc8fe0f3f0f33ad9
odajunpei <i.penju1022@gmail.com>
2022-02-19 15:33:09 +0900

    Initial

---
+++ app/Http/Controllers/GreetingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GreetingController extends Controller
+{
+    //add
+    public function index()
+    {
+        return view('greeting.index');
+    }
+
+    public function welcome(Request $request)
+    {
+        $rule = [
+            'username' => 'required|max:20'
+        ];
+        $request->validate($rule);
+        $username = $request->input('username');
+        return view('greeting.welcome', ['username' => $username]);
+    }
+}
